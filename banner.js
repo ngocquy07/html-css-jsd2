c45731801b13fef07da7506b69982fcceb784ee0
ngocquy07 <bocute712@gmail.com>
2025-05-20 00:42:49 +0700

    html-css-js

---
+++ banner.js
@@ -0,0 +1,16 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.banner-slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 2000);
+
